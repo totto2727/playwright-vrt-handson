a8af11165da39e4e06b966ea49ddbd509068333b
totto2727 <h_tsuchida@yumemi.co.jp>
2024-04-22 19:02:29 +0900

    feat: 事例ページのテストを追加

---
+++ tests/jirei.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from "@playwright/test";
+
+test("制作事例のVRT", async ({ page }) => {
+  await page.goto("/jirei.html");
+  await expect(page).toHaveScreenshot();
+});
+
+test("制作事例のページ全体のVRT", async ({ page }) => {
+  await page.goto("/jirei.html");
+  await expect(page).toHaveScreenshot({ fullPage: true });
+});

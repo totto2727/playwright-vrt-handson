e72b8a1686430455c23f55bb109bd8d68f4c530c
totto2727 <h_tsuchida@yumemi.co.jp>
2024-04-22 19:03:28 +0900

    feat: ホームページのテストを追加

---
+++ tests/index.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from "@playwright/test";
+
+function sleep() {
+  return new Promise(function (resolve) {
+    setTimeout(function () {
+      resolve(null);
+    }, (Math.random() + 0.5) * 10000);
+  });
+}
+
+test("制作事例のキービジュアルのVRT", async ({ page }) => {
+  await page.goto("/");
+  const kv = page.locator("#mainimg-box");
+  // 少し時間のかかる処理
+  await sleep();
+  await expect(page).toHaveScreenshot({ mask: [kv] });
+});
